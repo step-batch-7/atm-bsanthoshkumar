0d968a3a450b665d032c0b49a66ee47ba13bef12
santhosh <santhosh.kumar@thoughtworks.com>
2020-04-07 17:17:57 +0530

    added print_denominations _count function which takes the all notes count and print count of each denominations
    modified get_money function

--- atm.c
+++ atm.c
@@ -2,17 +2,21 @@
 
 unsigned int get_money(unsigned short int amount)
 {
-  int denominations[] = {2000, 500, 100, 50, 20, 10, 5, 1};
+  if(amount > 31999)
+  {
+    return 0;
+  }
+
+  int denominations[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
   unsigned int total_notes = 0X0;
   unsigned short int balance = amount;
-  if(balance <= 31999)
+
+  for(int i = 0;i < 8;i++)
   {
-    for(int i = 0;i < 8;i++)
-    {
-      total_notes = total_notes << 4;
-      total_notes += balance / denominations[i];
-      balance = balance % denominations[i];
-    }
+    total_notes = total_notes << 4;
+    total_notes += balance / denominations[i];
+    balance %= denominations[i];
   }
+  
   return total_notes;
 }
--- main.c
+++ main.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include "atm.h"
 
+int print_denominations_count(unsigned int notes)
+{
+  int denominations[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+  for(int i = 0;i < 8;i++)
+    {
+      printf("\t%d notes of %d\n",notes >> 28,denominations[i]);
+      notes = notes << 4;
+    }
+  
+  return 0;
+}
+
 int main(void)
 {
   unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
   for (int i = 0; i < 6; i++)
   {
     unsigned int notes = get_money(amounts[i]);
-    printf("%5u %08x\n", amounts[i], notes);
+    printf("%5u %0x\n", amounts[i],notes);
+    print_denominations_count(notes);
+    printf("\n");
   }
 
   return 0;
