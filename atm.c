aca67815132089465cc037ceda09f3d744fd7820
santhosh <santhosh.kumar@thoughtworks.com>
2020-04-07 15:49:38 +0530

    added code for get_money funciton which takes the amount and gives the no of notes for each denominations

--- atm.c
+++ atm.c
@@ -1,4 +1,15 @@
-unsigned int get_money(unsigned short int money)
+#include <stdio.h>
+
+unsigned int get_money(unsigned short int amount)
 {
-  return 0;
+  int denominations[] = {2000, 500, 100, 50, 20, 10, 5, 1};
+  unsigned int total_notes = 0X0;
+  unsigned short int balance = amount;
+  for(int i = 0;i < 8;i++)
+  {
+    total_notes = total_notes << 4;
+    total_notes += balance / denominations[i];
+    balance = balance % denominations[i];
+  }
+  return total_notes;
 }
