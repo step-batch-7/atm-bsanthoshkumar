cbc426b6c4082882b2fe06d68e62588d1abafefe
santhosh <santhosh.kumar@thoughtworks.com>
2020-04-07 15:58:45 +0530

    added condition for given amount more than 31999 which returns 0

--- atm.c
+++ atm.c
@@ -5,11 +5,14 @@ unsigned int get_money(unsigned short int amount)
   int denominations[] = {2000, 500, 100, 50, 20, 10, 5, 1};
   unsigned int total_notes = 0X0;
   unsigned short int balance = amount;
-  for(int i = 0;i < 8;i++)
+  if(balance <= 31999)
   {
-    total_notes = total_notes << 4;
-    total_notes += balance / denominations[i];
-    balance = balance % denominations[i];
+    for(int i = 0;i < 8;i++)
+    {
+      total_notes = total_notes << 4;
+      total_notes += balance / denominations[i];
+      balance = balance % denominations[i];
+    }
   }
   return total_notes;
 }
